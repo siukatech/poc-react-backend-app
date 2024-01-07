499358bd88b4fed047214858ddac09f6eeb5bcbc
siukatech <siuka114@gmail.com>
2024-01-07 16:11:08 +0800

    change to register mapper of form and entity dynamically

--- src/main/java/com/siukatech/poc/react/backend/app/config/AppConfig.java
+++ src/main/java/com/siukatech/poc/react/backend/app/config/AppConfig.java
@@ -1,19 +1,23 @@
 package com.siukatech.poc.react.backend.app.config;
 
-import com.siukatech.poc.react.backend.app.data.entity.I18nEntity;
-import com.siukatech.poc.react.backend.app.data.entity.ItemEntity;
-import com.siukatech.poc.react.backend.app.data.entity.MerchantEntity;
-import com.siukatech.poc.react.backend.app.business.form.I18nForm;
-import com.siukatech.poc.react.backend.app.business.form.ItemForm;
-import com.siukatech.poc.react.backend.app.business.form.MerchantForm;
 import com.siukatech.poc.react.backend.parent.EnableReactBackendParent;
+import com.siukatech.poc.react.backend.parent.business.form.AbstractForm;
+import com.siukatech.poc.react.backend.parent.data.entity.AbstractEntity;
 import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import org.apache.commons.lang3.ClassUtils;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
+import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 //@Import({
@@ -25,6 +29,7 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 
     /**
      * Dont setup the webMvcConfigurer in child microservices
@@ -88,50 +93,81 @@ public class AppConfig {
 
     @PostConstruct
     public void configModelMapper() {
+////        this.modelMapper.addMappings(new PropertyMap<I18nForm, I18nEntity>() {
+////            protected void configure() {
+////                skip().setId(null);
+////            }
+////        });
 //        this.modelMapper.addMappings(new PropertyMap<I18nForm, I18nEntity>() {
 //            protected void configure() {
 //                skip().setId(null);
 //            }
 //        });
-        this.modelMapper.addMappings(new PropertyMap<I18nForm, I18nEntity>() {
-            protected void configure() {
-                skip().setId(null);
-            }
-        });
-        try {
-            boolean isExisted = this.modelMapper.getTypeMaps().stream()
-                    .anyMatch(typeMap -> {
-                        logger.debug("configModelMapper - typeMap.getName: [" + typeMap.getName()
-                                + "], typeMap.getMappings.size: [" + typeMap.getMappings().size()
-                                + "]");
-                        typeMap.getMappings().stream().forEach(mapping -> {
-                            logger.debug("configModelMapper - mapping.getSourceType.getName: [" + mapping.getSourceType().getName()
-                                    + "], mapping.getPath: [" + mapping.getPath()
-                                    + "], mapping.toString: [" + mapping.toString()
-                                    + "]");
-                        });
-//                        boolean result = typeMap.equals(this.skipItemFormItemEntityModifiedFieldsMap);
-                        boolean result = true;
-                        return result;
-                    });
-            logger.debug("configModelMapper - isExisted: [" + isExisted
-                    + "]");
-            if (!isExisted) {
-                this.modelMapper.addMappings(new PropertyMap<ItemForm, ItemEntity>() {
-                    protected void configure() {
-                        skip().setId(null);
-                    }
-                });
-            }
-        } catch (Exception e) {
-            logger.error("configModelMapper - e.getMessage: [" + e.getMessage() + "]", e);
-        }
-        this.modelMapper.addMappings(new PropertyMap<MerchantForm, MerchantEntity>() {
-            protected void configure() {
-                skip().setId(null);
+//        try {
+//            boolean isExisted = this.modelMapper.getTypeMaps().stream()
+//                    .anyMatch(typeMap -> {
+//                        logger.debug("configModelMapper - typeMap.getName: [" + typeMap.getName()
+//                                + "], typeMap.getMappings.size: [" + typeMap.getMappings().size()
+//                                + "]");
+//                        typeMap.getMappings().stream().forEach(mapping -> {
+//                            logger.debug("configModelMapper - mapping.getSourceType.getName: [" + mapping.getSourceType().getName()
+//                                    + "], mapping.getPath: [" + mapping.getPath()
+//                                    + "], mapping.toString: [" + mapping.toString()
+//                                    + "]");
+//                        });
+////                        boolean result = typeMap.equals(this.skipItemFormItemEntityModifiedFieldsMap);
+//                        boolean result = true;
+//                        return result;
+//                    });
+//            logger.debug("configModelMapper - isExisted: [" + isExisted
+//                    + "]");
+//            if (!isExisted) {
+//                this.modelMapper.addMappings(new PropertyMap<ItemForm, ItemEntity>() {
+//                    protected void configure() {
+//                        skip().setId(null);
+//                    }
+//                });
+//            }
+//        } catch (Exception e) {
+//            logger.error("configModelMapper - e.getMessage: [" + e.getMessage() + "]", e);
+//        }
+//        this.modelMapper.addMappings(new PropertyMap<MerchantForm, MerchantEntity>() {
+//            protected void configure() {
+//                skip().setId(null);
+//            }
+//        });
+
+        // https://www.baeldung.com/java-scan-annotations-runtime
+        Reflections reflections = new Reflections(this.getClass().getPackageName());
+        Set<Class<?>> entityTypes = reflections.getTypesAnnotatedWith(Entity.class);
+        entityTypes.forEach(entityType -> {
+            String formTypeName = entityType.getName().replaceFirst("Entity", "Form");
+            try {
+                Class formType = ClassUtils.getClass(formTypeName);
+                AbstractEntity entityObj = (AbstractEntity) entityType.getDeclaredConstructor().newInstance();
+                AbstractForm formObj = (AbstractForm) formType.getDeclaredConstructor().newInstance();
+                registerPropertyMap(formObj, entityObj);
+            } catch (ClassNotFoundException
+                     | InvocationTargetException
+                     | InstantiationException
+                     | IllegalAccessException
+                     | NoSuchMethodException
+                    e) {
+                //throw new RuntimeException(e);
+                // do nothing
+                logger.error("configModelMapper - e.getMessage: " + e.getMessage(), e);
             }
         });
 
+    }
+
+    private <F extends AbstractForm, E extends AbstractEntity> void registerPropertyMap(
+            F abstractForm, E abstractEntity) {
+        this.modelMapper.addMappings(new PropertyMap<F, E>() {
+            protected void configure() {
+                skip().setId(null);
+            }
+        });
     }
 
 }
