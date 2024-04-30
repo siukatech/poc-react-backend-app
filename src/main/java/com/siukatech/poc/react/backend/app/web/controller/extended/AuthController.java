f3ae497a8e9903c332137c68084770a6b70a9f96
siukatech <siuka114@gmail.com>
2024-04-30 22:12:58 +0800

    feat: add codeChallenge and codeVerifier for pkce implementation

--- src/main/java/com/siukatech/poc/react/backend/app/web/controller/extended/AuthController.java
+++ src/main/java/com/siukatech/poc/react/backend/app/web/controller/extended/AuthController.java
@@ -8,10 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URISyntaxException;
@@ -48,7 +45,7 @@ public class AuthController extends com.siukatech.poc.react.backend.parent.web.c
 
     @GetMapping(value = "/auth/login/{clientName}")
     public void doAuthCodeLogin(HttpServletResponse response
-            , @PathVariable String clientName) {
+            , @PathVariable String clientName, @RequestParam(required = false) String codeChallenge) {
 //        OAuth2ClientProperties.Registration registration = this.oAuth2ClientProperties.getRegistration().get(clientName);
 //        OAuth2ClientProperties.Provider provider = this.oAuth2ClientProperties.getProvider().get(clientName);
 ////        response_type=code&client_id=react-backend-client-01&scope=openid&redirect_uri=http://localhost:3000/redirect
@@ -66,57 +63,58 @@ public class AuthController extends com.siukatech.poc.react.backend.parent.web.c
 //                ;
 //        response.setHeader("Location", authUrl);
 //        response.setStatus(302);
-        super.doAuthCodeLogin(response, clientName);
+        super.doAuthCodeLogin(response, clientName, codeChallenge);
     }
 
-    @PostMapping(value = "/auth/token/{clientName}/{code}")
-    public ResponseEntity<?> doAuthCodeToken(@PathVariable String clientName
-            , @PathVariable String code) {
-//        OAuth2ClientProperties.Registration registration = this.oAuth2ClientProperties.getRegistration().get(clientName);
-//        OAuth2ClientProperties.Provider provider = this.oAuth2ClientProperties.getProvider().get(clientName);
-////        response_type=code&client_id=react-backend-client-01&scope=openid&redirect_uri=http://localhost:3000/redirect
-//
-//        // headers.set('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
-//        // headers.set('Authorization', `Basic ${Buffer.from(`${client_id}:${client_secret}`)}`);
-//
-//        String tokenUrl = provider.getTokenUri();
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        String basicAuthorization = registration.getClientId() + ":" + registration.getClientSecret();
-//        httpHeaders.set(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(basicAuthorization.getBytes(StandardCharsets.UTF_8)) + "");
-//        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
-////        TokenReq tokenReq = new TokenReq(registration.getClientId(), registration.getClientSecret()
-////                , registration.getRedirectUri(), registration.getAuthorizationGrantType(), code);
-//        MultiValueMap<String, String> tokenReq = new LinkedMultiValueMap<String, String>();
-//        tokenReq.add("client_id", registration.getClientId());
-//        tokenReq.add("client_secret", registration.getClientSecret());
-//        tokenReq.add("redirect_uri", registration.getRedirectUri());
-//        tokenReq.add("grant_type", registration.getAuthorizationGrantType());
-//        tokenReq.add("code", code);
-//        HttpEntity<?> httpEntity = new HttpEntity<>(tokenReq, httpHeaders);
-//
-//        log.debug("doAuthCodeToken - clientName: [" + clientName
-//                + "], code: [" + code
-//                + "], tokenReq: [" + tokenReq
-//                + "], httpHeaders: [" + httpHeaders
-//                + "], oauth2ClientRestTemplate.toString: [" + oauth2ClientRestTemplate.toString()
-//                + "], oauth2ClientRestTemplate.getMessageConverters.size: [" + oauth2ClientRestTemplate.getMessageConverters().size()
-//                + "]");
-//        oauth2ClientRestTemplate.getMessageConverters().stream().forEach(httpMessageConverter -> {
-//            log.debug("doAuthCodeToken - httpMessageConverter.getClass.getName: [" + httpMessageConverter.getClass().getName() + "]");
-//        });
-//
-//        ResponseEntity<TokenRes> responseEntity = oauth2ClientRestTemplate.exchange(tokenUrl
-//                , HttpMethod.POST, httpEntity, TokenRes.class);
-//
-//        log.debug("doAuthCodeToken - clientName: [" + clientName
-//                + "], code: [" + code
-//                + "], tokenReq: [" + tokenReq
-//                + "], responseEntity: [" + responseEntity
-//                + "]");
-//
-//        return ResponseEntity.ok(responseEntity.getBody());
-        return super.doAuthCodeToken(clientName, code);
-    }
+//    @PostMapping(value = "/auth/token/{clientName}/{code}")
+//    public ResponseEntity<?> doAuthCodeToken(@PathVariable String clientName
+//            , @PathVariable String code, @RequestParam(required = false) String codeVerifier) {
+//        log.debug("doAuthCodeToken - clientName: [{}], code: [{}], codeVerifier: [{}]", clientName, code, codeVerifier);
+////        OAuth2ClientProperties.Registration registration = this.oAuth2ClientProperties.getRegistration().get(clientName);
+////        OAuth2ClientProperties.Provider provider = this.oAuth2ClientProperties.getProvider().get(clientName);
+//////        response_type=code&client_id=react-backend-client-01&scope=openid&redirect_uri=http://localhost:3000/redirect
+////
+////        // headers.set('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+////        // headers.set('Authorization', `Basic ${Buffer.from(`${client_id}:${client_secret}`)}`);
+////
+////        String tokenUrl = provider.getTokenUri();
+////        HttpHeaders httpHeaders = new HttpHeaders();
+////        String basicAuthorization = registration.getClientId() + ":" + registration.getClientSecret();
+////        httpHeaders.set(HttpHeaders.AUTHORIZATION, "Basic " + Base64.getEncoder().encodeToString(basicAuthorization.getBytes(StandardCharsets.UTF_8)) + "");
+////        httpHeaders.set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+//////        TokenReq tokenReq = new TokenReq(registration.getClientId(), registration.getClientSecret()
+//////                , registration.getRedirectUri(), registration.getAuthorizationGrantType(), code);
+////        MultiValueMap<String, String> tokenReq = new LinkedMultiValueMap<String, String>();
+////        tokenReq.add("client_id", registration.getClientId());
+////        tokenReq.add("client_secret", registration.getClientSecret());
+////        tokenReq.add("redirect_uri", registration.getRedirectUri());
+////        tokenReq.add("grant_type", registration.getAuthorizationGrantType());
+////        tokenReq.add("code", code);
+////        HttpEntity<?> httpEntity = new HttpEntity<>(tokenReq, httpHeaders);
+////
+////        log.debug("doAuthCodeToken - clientName: [" + clientName
+////                + "], code: [" + code
+////                + "], tokenReq: [" + tokenReq
+////                + "], httpHeaders: [" + httpHeaders
+////                + "], oauth2ClientRestTemplate.toString: [" + oauth2ClientRestTemplate.toString()
+////                + "], oauth2ClientRestTemplate.getMessageConverters.size: [" + oauth2ClientRestTemplate.getMessageConverters().size()
+////                + "]");
+////        oauth2ClientRestTemplate.getMessageConverters().stream().forEach(httpMessageConverter -> {
+////            log.debug("doAuthCodeToken - httpMessageConverter.getClass.getName: [" + httpMessageConverter.getClass().getName() + "]");
+////        });
+////
+////        ResponseEntity<TokenRes> responseEntity = oauth2ClientRestTemplate.exchange(tokenUrl
+////                , HttpMethod.POST, httpEntity, TokenRes.class);
+////
+////        log.debug("doAuthCodeToken - clientName: [" + clientName
+////                + "], code: [" + code
+////                + "], tokenReq: [" + tokenReq
+////                + "], responseEntity: [" + responseEntity
+////                + "]");
+////
+////        return ResponseEntity.ok(responseEntity.getBody());
+//        return super.doAuthCodeToken(clientName, code, codeVerifier);
+//    }
 
     @PostMapping(value = "/auth/login/{clientName}")
     public ResponseEntity<?> doPasswordLogin(@PathVariable String clientName
