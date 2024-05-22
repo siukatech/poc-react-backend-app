95d97ada9b93e1eda1535566c9feb995cd0c41b3
siukatech <siuka114@gmail.com>
2024-05-22 12:35:36 +0800

    feat: add FigureShfRepository

--- src/main/java/com/siukatech/poc/react/backend/app/figure/business/service/FigureFigmaService.java
+++ src/main/java/com/siukatech/poc/react/backend/app/figure/business/service/FigureFigmaService.java
@@ -1,12 +1,20 @@
 package com.siukatech.poc.react.backend.app.figure.business.service;
 
 import com.siukatech.poc.react.backend.app.figure.data.entity.FigureFigmaEntity;
+import com.siukatech.poc.react.backend.app.figure.data.repository.FigureFigmaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class FigureFigmaService extends FigureBaseService<FigureFigmaEntity> {
+
+    private final FigureFigmaRepository figureFigmaRepository;
+
+    public FigureFigmaService(FigureFigmaRepository figureFigmaRepository) {
+        this.figureFigmaRepository = figureFigmaRepository;
+    }
+
     @Override
     public FigureFigmaEntity prepareFigureBase(FigureFigmaEntity figureBaseEntity) {
         log.debug("prepareFigureBase - figureBaseEntity: [{}]", figureBaseEntity);
--- src/main/java/com/siukatech/poc/react/backend/app/figure/business/service/FigureShfService.java
+++ src/main/java/com/siukatech/poc/react/backend/app/figure/business/service/FigureShfService.java
@@ -1,12 +1,20 @@
 package com.siukatech.poc.react.backend.app.figure.business.service;
 
 import com.siukatech.poc.react.backend.app.figure.data.entity.FigureShfEntity;
+import com.siukatech.poc.react.backend.app.figure.data.repository.FigureShfRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class FigureShfService extends FigureBaseService<FigureShfEntity> {
+
+    private final FigureShfRepository figureShfRepository;
+
+    public FigureShfService(FigureShfRepository figureShfRepository) {
+        this.figureShfRepository = figureShfRepository;
+    }
+
     @Override
     public FigureShfEntity prepareFigureBase(FigureShfEntity figureBaseEntity) {
         log.debug("prepareFigureBase - figureBaseEntity: [{}]", figureBaseEntity);
