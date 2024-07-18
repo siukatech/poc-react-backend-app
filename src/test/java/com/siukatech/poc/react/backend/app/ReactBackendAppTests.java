f3b7806157986708210656408c6cbd1edaf4b6c5
siukatech <siuka114@gmail.com>
2024-07-19 07:19:24 +0800

    feat: use SpringBootTest with required class members

--- src/test/java/com/siukatech/poc/react/backend/app/ReactBackendAppTests.java
+++ src/test/java/com/siukatech/poc/react/backend/app/ReactBackendAppTests.java
@@ -1,7 +1,14 @@
 package com.siukatech.poc.react.backend.app;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
+@SpringBootTest
 //@SpringBootTest(properties = {
 //		"client-id=XXX"
 //		, "client-secret=XXX"
@@ -13,6 +20,15 @@ import org.junit.jupiter.api.Test;
 //@OverrideAutoConfiguration(enabled = false)
 //@AutoConfigureDataJpa
 class ReactBackendAppTests {
+
+	@MockBean
+	private OAuth2ClientProperties oAuth2ClientProperties;
+	@MockBean
+	private InMemoryClientRegistrationRepository clientRegistrationRepository;
+	@MockBean
+	private OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
+	@MockBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
