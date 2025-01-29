a767e4665f8b7fc7b476504910f390b2ea878d19
siukatech <siuka114@gmail.com>
2025-01-29 23:31:19 +0800

    feat: comment out unnecessary components after update of WebSecurityConfig

--- src/test/java/com/siukatech/poc/react/backend/app/ReactBackendAppTests.java
+++ src/test/java/com/siukatech/poc/react/backend/app/ReactBackendAppTests.java
@@ -23,13 +23,16 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 class ReactBackendAppTests {
 
 	@MockBean
-	private OAuth2ClientProperties oAuth2ClientProperties;
-	@MockBean
 	private InMemoryClientRegistrationRepository clientRegistrationRepository;
-	@MockBean
-	private OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
-	@MockBean
-	private JwtDecoder jwtDecoder;
+
+	// After moving the following WebSecurityConfig Components from constructor to filterChain,
+	// they are no longer required to mark as MockBean or MockitoBean
+//	@MockBean
+//	private OAuth2ClientProperties oAuth2ClientProperties;
+//	@MockBean
+//	private OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
+//	@MockBean
+//	private JwtDecoder jwtDecoder;
 	// @MockBean
 	// private ProblemDetailExtMapper problemDetailExtMapper;
 
